97ec9dcb0de5acbb5ee2fc90b83390bf9fed0398
pbemerk <pbemerk@gmail.com>
2020-02-03 02:23:32 -0500

    Prompt Test 02

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 var message = "Testing Prompts!"
 alert(message);
-prompt("What is your name?");
-message = "Using Prompts in JavaScript!";
-alert(message);
+var visitorName = prompt("What is your name?");
+message = " We're Using Prompts in JavaScript!";
+alert(visitorName + message);
 document.write("Looks like prompts are working");
 console.log("Program Complete");
