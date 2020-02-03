0e60e867ead24b015c99f9410c0509c895f624ad
pbemerk <pbemerk@gmail.com>
2020-02-03 02:00:06 -0500

    Variable Test 02

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 var message = "Testing Variables!"
 alert(message);
+message = "Changing Variables in JavaScript";
+alert(message);
 document.write("Looks like variables are working");
 console.log("Program Complete");
