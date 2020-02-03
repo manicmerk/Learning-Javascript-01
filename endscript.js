6a6440eb82f71ea16e8dcf8c3a2cb60743aab5d9
pbemerk <pbemerk@gmail.com>
2020-02-03 03:19:24 -0500

    Prompt, combining vars, 2nd script for timed end messages

---
+++ endscript.js
@@ -0,0 +1,3 @@
+var message = visitorName + " we did it!"
+setTimeout(function() { alert(message); }, 10000 );
+setTimeout(function() { alert("Great Job!"); }, 10000 );
--- scripts.js
+++ scripts.js
@@ -6,4 +6,8 @@ alert(visitorName + message);
 message = "<p>" + visitorName + " can you believe we're concatenating?!?</p>"
 document.write("<p>Looks like prompts are working!</p>");
 document.write(message);
+message = "<p>" + visitorName + " let's try the more complex message+=</p>" + "<br>" + "<br>"
+message += "Will this actually work?" + "<br>" + "<br>" 
+message += "<p>" + visitorName + " if we see this, then it did!</p>"
+document.write(message);
 console.log("Program Complete");
