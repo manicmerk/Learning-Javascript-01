15db787115aa43a560a7f8a2485dd6f94b696ad5
pbemerk <pbemerk@gmail.com>
2020-02-03 20:12:49 -0500

    adjusted font sizes, changed delay alert to 17 sec

--- endscript.js
+++ endscript.js
@@ -1,10 +1,10 @@
 var message = visitorName + " we did it!"
-setTimeout(function() { alert(message); }, 10000 );
-setTimeout(function() { alert("Great Job!"); }, 10000 );
+setTimeout(function() { alert(message); }, 17000 );
+setTimeout(function() { alert("Great Job!"); }, 17000 );
 console.log(message.toLowerCase());
 console.log(message.toUpperCase());
 
 var passphrase = "Colts";
 console.log(passphrase.length);
 console.log(passphrase.toLowerCase());
-console.log(passphrase.toUpperCase());
+console.log(passphrase.toUpperCase()); 
--- scripts.js
+++ scripts.js
@@ -6,8 +6,12 @@ alert(visitorName + message);
 message = "<p>" + visitorName + " can you believe we're concatenating?!?</p>"
 document.write("<p>Looks like prompts are working!</p>");
 document.write(message);
-message = "<p>" + visitorName + " let's try the more complex message+=</p>" + "<br>" + "<br>"
-message += "Will this actually work?" + "<br>" + "<br>" 
-message += "<p>" + visitorName + " if we see this, then it did!</p>"
+message = "<p>" + visitorName + " let's try the more complex message+=</p>"
+message += "<p>And we can try a fontsize string on the next lines too!</p><br><br>"
+document.write(message)
+message = "Will this actually work?" + "<br>" + "<br>"
+document.write(message)
+document.write(message.fontsize(5))
+message = "<p>" + visitorName + " if we see this, then it did!</p>"
 document.write(message);
-console.log("Program Complete");
+console.log("Program Complete"); 
